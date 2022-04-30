a097d0434c0f0d36eae9c0f493175a77ace1f8f0
moayad.okal <moayad.okal@ovgu.de>
2022-04-30 15:50:27 +0200

    Task 2

--- src/reactive_behaviour/reactive_behaviour/controller.py
+++ src/reactive_behaviour/reactive_behaviour/controller.py
@@ -17,8 +17,19 @@ class VelocityController(Node):
     def timer_cb(self):
         msg = Twist()
         x = self.forward_distance - 0.3
-        x = x if x < 0.1 else 0.1
-        x = x if x >= 0 else 0.0
+        z = 0.0
+        if x < 0.1:
+            x = x 
+        else: 
+            x=0.1
+
+        if x >= 0:
+            x = x
+        else: 
+            x=x
+            z=0.1
+
+        msg.angular.z = z
         msg.linear.x = x
         self.publisher.publish(msg)
     
@@ -42,3 +53,62 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
+# import rclpy
+# from rclpy.node import Node
+
+# from geometry_msgs.msg import Twist
+# from sensor_msgs.msg import LaserScan
+
+# class VelocityController(Node):
+
+#     def init(self):
+#         super().init('velocity_controller')
+#         self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
+#         self.forward_distance = 0
+#         self.left_distance = 0
+#         self.right_distance = 0
+#         self.backward_distance = 0
+#         self.create_subscription(LaserScan, 'scan', self.laser_cb, rclpy.qos.qos_profile_sensor_data)
+#         self.create_timer(0.1, self.timer_cb)
+#         self.get_logger().info('controller node started')
+
+#     def timer_cb(self):
+#         msg = Twist()
+
+#         if self.forward_distance > 0.1 and self.backward_distance>0.1 and self.left_distance>0.1 and self.right_distance>0.1:
+#             msg.linear.x=0.2
+#             msg.angular.z=0.2
+#         else:
+#             msg.angular.z =0.1
+#             if self.forward_distance > 0.4 and self.backward_distance>0.4 and self.left_distance>0.1 and self.right_distance>0.1:
+#                 msg.linear.x=0.1
+#                 msg.angular.z=0.3
+
+#         self.publisher.publish(msg)
+
+
+
+#     def laser_cb(self, msg):
+#         self.forward_distance = msg.ranges[0]
+#         self.backward_distance = msg.ranges[360]
+#         self.left_distance =msg.ranges[270]
+#         self.right_distance = msg.ranges[90]
+
+
+
+# def main(args=None):
+#     rclpy.init(args=args)
+
+#     node = VelocityController()
+
+#     rclpy.spin(node)
+
+#     # Destroy the node explicitly
+#     # (optional - otherwise it will be done automatically
+#     # when the garbage collector destroys the node object)
+#     node.destroy_node()
+#     rclpy.shutdown()
+
+# if __name__ == '__main__':
+#     main()
--- src/watchdog/watchdog/watchdog.py
+++ src/watchdog/watchdog/watchdog.py
@@ -10,17 +10,42 @@ class WatchdogNode(Node):
         super().__init__('watchdog')
         self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
         self.create_subscription(Twist, 'input_cmd', self.cmd_callback, 10)
+        self.twist = Twist()
+        self.msg=''
         self.create_subscription(String, 'controller_cmd', self.controller_callback, 10)
         self.get_logger().info('Watchdog node started')
 
     def cmd_callback(self, msg):
         # this makes the turle go backwards
         # (just so you know its working)
-        msg.linear.x = -1 * msg.linear.x
-        self.get_logger().info(f'msg.linear.x: {msg.linear.x}')
+        if self.msg=="start":
+            # self.twist.linear.x = 0.0
+            # self.twist.linear.y = 0.0
+            # self.twist.linear.z = 0.0
+            # self.twist.angular.x = 0.0
+            # self.twist.angular.y = 0.0
+            self.twist.angular.z = 0.0
+            # msg.linear.x = self.twist.linear.x
+            # msg.linear.z = self.twist.angular.x
+            # msg.linear.x = self.twist.linear.y
+            # msg.linear.z = self.twist.angular.y
+            # msg.linear.x = self.twist.linear.z
+            msg.linear.x = -1 * msg.linear.x
+            msg.angular.z = self.twist.angular.z
+        elif self.msg=="stop":
+            self.twist.linear.x =0.0
+            self.twist.angular.z = 0.0
+            msg.linear.x = self.twist.linear.x
+            msg.angular.z = self.twist.angular.z
+        else:
+            # self.twist.linear.x = 1
+            # self.twist.angular.z = 1
+            msg.linear.x = -1 * msg.linear.x
+            # self.get_logger().info(f'msg.linear.x: {msg.linear.x}')
         self.publisher.publish(msg)
         
     def controller_callback(self, msg):
+        self.msg=msg.data
         self.get_logger().warn(f'The controller says I should {msg.data} the turtle ...')
 
 
